d629a7c7187f0974b82f308b829ffbf7d1ae904a
daz2345 <darrenimpey@btinternet.com>
2015-08-11 13:15:26 +0000

    Moved IP Blocker to local

--- packages/dh-whitelist/package.js
+++ packages/dh-whitelist/package.js
@@ -8,7 +8,15 @@ Package.onUse(function (api) {
 
   // ---------------------------------- 1. Core dependency -----------------------------------
 
-  api.use("cybit:ip-blocker@1.0.4");
+    api.versionsFrom('METEOR@1.0.2');
+    api.use('iron:router@1.0.5', 'server');
+    api.use('webapp', 'server');
+    api.use('underscore','server');
+    // make sure we come after livedata, so we load after the sockjs
+    // server has been instantiated.
+    api.add_files('lib/server/ip_blocker_config.js', 'server');
+    api.export('IP_BLOCKER', 'server');
+    api.add_files('lib/server/ip_blocker.js', 'server');
 
   // ---------------------------------- 2. Files to include ----------------------------------
 

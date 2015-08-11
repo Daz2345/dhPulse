9fb944bee7695d530194a0368719d711f4ec5e0a
daz2345 <darrenimpey@btinternet.com>
2015-08-11 14:37:54 +0000

    UPdate to tags sort order

--- packages/telescope-tags/lib/server/publications.js
+++ packages/telescope-tags/lib/server/publications.js
@@ -1,6 +1,6 @@
 Meteor.publish('categories', function() {
   if(Users.can.viewById(this.userId)){
-    return Categories.find();
+    return Categories.find({}, {sort: {name: 1}});
   }
   return [];
 });

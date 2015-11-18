05580f9846aa2e1fc6b90eb4202834effc9bf852
daz2345 <darrenimpey@btinternet.com>
2015-11-18 14:40:39 +0000

    cahnge to category parameters

--- packages/dh-personal/lib/parameters.js
+++ packages/dh-personal/lib/parameters.js
@@ -28,28 +28,28 @@ function userCategories (parameters, terms) {
 }
 Telescope.callbacks.add("postsParameters", userCategories);
 
-Telescope.callbacks.remove("postsParameters", "addCategoryParameter");
+// Telescope.callbacks.remove("postsParameters", "addCategoryParameter");
 
-function addCategoryIntersectionParameter (parameters, terms) {
-  // filter by category if category slugs are provided
-  if (!!terms.cat) {
+// function addCategoryIntersectionParameter (parameters, terms) {
+//   // filter by category if category slugs are provided
+//   if (!!terms.cat) {
 
-    var categoriesIds = [];
-    var find = {};
+//     var categoriesIds = [];
+//     var find = {};
 
-    if (typeof terms.cat === "string") { // cat is a string
-      find = {slug: terms.cat};
-    } else { // cat is an array
-      find = {slug: {$in: terms.cat}};
-    }
+//     if (typeof terms.cat === "string") { // cat is a string
+//       find = {slug: terms.cat};
+//     } else { // cat is an array
+//       find = {slug: {$in: terms.cat}};
+//     }
 
-    // get all categories passed in terms
-    var categories = Categories.find(find).fetch();
+//     // get all categories passed in terms
+//     var categories = Categories.find(find).fetch();
 
-    var categoriesIds = _.pluck(categories, "_id");
+//     var categoriesIds = _.pluck(categories, "_id");
 
-    parameters.find.categories = {$all: categoriesIds};
-  }
-  return parameters;
-}
-Telescope.callbacks.add("postsParameters", addCategoryIntersectionParameter);
+//     parameters.find.categories = {$all: categoriesIds};
+//   }
+//   return parameters;
+// }
+// Telescope.callbacks.add("postsParameters", addCategoryIntersectionParameter);
